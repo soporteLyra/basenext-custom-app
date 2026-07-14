127d9aa1ff4352d17ac528a7f39fa1aff341c179
Jose Mª Pascual <jmpascual@lyra-informatica.es>
2026-07-14 19:57:18 +0200

    fix: estructura plana sin public/, app_name=erpnext_pos_extensions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="basenext",
+    name="erpnext-pos-extensions",
     version="0.1.0",
     description="Extensiones POS para ERPNext: vales, ticket regalo y personalizaciones BaseN",
     author="Lyra Informática",
